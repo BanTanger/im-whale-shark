d1e7929db1036f60ada2648385f78809ce359343
BanTanger <88583317+BanTanger@users.noreply.github.com>
2023-04-21 13:21:11 +0800

    update README.md

--- im-service/src/main/java/com/bantanger/im/service/strategy/command/BaseCommandStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.bantanger.im.service.strategy.command;
-
-/**
- * @author BanTanger 半糖
- * @Date 2023/3/25 9:52
- */
-public abstract class BaseCommandStrategy implements CommandStrategy {
-
-}
