3cc78d2e37504baf2e0d057212494521e25894a2
BanTanger <88583317+BanTanger@users.noreply.github.com>
2023-08-03 12:32:58 +0800

    fix: 完善部署脚本

--- docker/copy.sh
+++ docker/copy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 创建目标目录
-mkdir -p im-whale-shark/domain/jar
-mkdir -p im-whale-shark/message-store/jar
-mkdir -p im-whale-shark/tcp/jar
+mkdir -p build/domain/jar
+mkdir -p build/message-store/jar
+mkdir -p build/tcp/jar
 
 # 复制jar文件
 echo "begin copy jar "
-cp ../im-domain/target/im-domain-1.0-SNAPSHOT.jar im-whale-shark/domain/jar/im-domain.jar
-cp ../im-message-store/target/im-message-store-1.0-SNAPSHOT.jar im-whale-shark/message-store/jar/im-message-store.jar
-cp ../im-tcp/target/im-tcp-1.0-SNAPSHOT.jar  im-whale-shark/tcp/jar/im-tcp.jar
+cp ../im-domain/target/im-domain-1.0-SNAPSHOT.jar build/domain/jar/im-domain.jar
+cp ../im-message-store/target/im-message-store-1.0-SNAPSHOT.jar build/message-store/jar/im-message-store.jar
+cp ../im-tcp/target/im-tcp-1.0-SNAPSHOT.jar  build/tcp/jar/im-tcp.jar
 echo "end copy jar "
