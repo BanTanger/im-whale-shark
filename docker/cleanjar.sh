1d4ca4ca71e2667387734c7ffa7a13dea9c9df97
BanTanger <88583317+BanTanger@users.noreply.github.com>
2023-08-03 14:11:07 +0800

    fix: 完善部署脚本

--- docker/cleanjar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 删除jar文件
-echo "开始清理jar文件"
-rm -f ../build/domain/jar/im-domain.jar
-rm -f ../build/message-store/jar/im-message-store.jar
-rm -f ../build/tcp/jar/im-tcp.jar
-rm -rf ../build
-echo "清理完成"
