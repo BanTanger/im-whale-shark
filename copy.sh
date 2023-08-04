a56a0056719e9d8d1a57b33ede67674d30ab1f0d
BanTanger <88583317+BanTanger@users.noreply.github.com>
2023-08-04 13:24:50 +0800

    fix: 完善部署脚本

--- copy.sh
+++ copy.sh
@@ -2,21 +2,12 @@
 
 echo "--------------------------------"
 echo "::: Welcome to IM-WhaleShark :::"
-
 # 创建目标目录
 target_directories=(
     "docker/build/domain"
     "docker/build/message-store"
     "docker/build/tcp"
 )
-
-for dir in "${target_directories[@]}"; do
-    echo "开始创建目标目录 $dir .."
-    mkdir -p "$dir" || echo "创建目标目录 $dir 失败"
-    echo "创建目标目录 $dir 完成"
-done
-
-echo ">>>>>>>>>>>>>>>>>"
 
 # 复制 Dockerfile 文件
 docker_files=(
@@ -25,54 +16,35 @@ docker_files=(
     "im-tcp/Dockerfile"
 )
 
-for index in "${!docker_files[@]}"; do
-    target_dir="${target_directories[$index]}"
-    target_file="$target_dir/$(basename "$index")"
-    docker_file="${docker_files[$index]}"
-    echo "开始复制 $docker_file .."
-    cp "$docker_file" "$target_file" || echo "复制 $docker_file 到 $target_file" 失败"
-    echo "复制 $docker_file 到 $target_file" 完成"
+for dir in "${target_directories[@]}"; do
+    echo "开始创建目标目录 $dir/jar .."
+    mkdir -p "$dir/jar"
+    echo "创建目标目录 $dir/jar 完成"
 done
 
 echo ">>>>>>>>>>>>>>>>>"
 
-# 复制 jar 文件
+for index in "${!docker_files[@]}"; do
+    target_file="${target_directories[$index]}/$(basename "${docker_files[$index]}")"
+    echo "开始复制 ${docker_files[$index]} .."
+    cp "${docker_files[$index]}" "$target_file"
+    echo "复制 ${docker_files[$index]} 到 $target_file 完成"
+done
+
+echo ">>>>>>>>>>>>>>>>>"
+
+# 复制 jar 文件并更改文件名
 jar_files=(
-    "im-domain/target/im-domain"
-    "im-message-store/target/im-message-store"
-    "im-tcp/target/im-tcp"
+    "im-domain/target/im-domain-1.0-SNAPSHOT.jar"
+    "im-message-store/target/im-message-store-1.0-SNAPSHOT.jar"
+    "im-tcp/target/im-tcp-1.0-SNAPSHOT.jar"
 )
 
-for file in "${jar_files[@]}"; do
-    target_dir="${target_directories[$file]}"
-    target_file="$target_dir/$(basename "$file").jar"
-    echo "开始复制 $file-1.0-SNAPSHOT .."
-    cp "$file-1.0-SNAPSHOT.jar" "$target_file" || echo "复制 $file-1.0-SNAPSHOT.jar 到 $target_file 失败"
-    echo "复制 $file-1.0-SNAPSHOT 到 $target_file 完成"
-done
-
-echo ">>>>>>>>>>>>>>>>>"
-
-# 展示创建的文件夹名称
-echo "以下文件夹已被创建:"
-for dir in "${target_directories[@]}"; do
-    echo "$dir"
-done
-
-echo ">>>>>>>>>>>>>>>>>"
-
-# 展示移动过程
-echo "以下文件已被移动:"
-for file in "${docker_files[@]}"; do
-    target_file="docker/build/$(basename "$file")"
-    echo "$file -> $target_file"
-done
-
-echo ">>>>>>>>>>>>>>>>>"
-
-# 展示复制过程
-echo "以下 jar 文件已被复制:"
-for file in "${jar_files[@]}"; do
-    target_file="docker/build/$(basename "$file")"
-    echo "$file -> $target_file"
+for index in "${!jar_files[@]}"; do
+    target_file="${target_directories[$index]}/jar/$(basename "${jar_files[$index]}")"
+    target_file_without_version="${target_directories[$index]}/jar/$(basename "${jar_files[$index]}" "-1.0-SNAPSHOT.jar").jar"
+    echo "开始复制 ${jar_files[$index]} .."
+    cp "${jar_files[$index]}" "$target_file"
+    mv "$target_file" "$target_file_without_version"
+    echo "复制 ${jar_files[$index]} 到 $target_file_without_version 完成"
 done
