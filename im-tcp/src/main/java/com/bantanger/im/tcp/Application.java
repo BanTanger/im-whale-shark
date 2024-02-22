43bc2135a9768fe1f2c77ce86f4c9e6be08ae644
BanTanger <1290288968@qq.com>
2024-02-22 11:29:53 +0800

    style: 调整结构

--- im-tcp/src/main/java/com/bantanger/im/tcp/Application.java
+++ im-tcp/src/main/java/com/bantanger/im/tcp/Application.java
@@ -2,7 +2,6 @@ package com.bantanger.im.tcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.support.GenericApplicationContext;
 
 /**
  * =================================== IM-WhaleShark ========================================
@@ -16,18 +15,11 @@ import org.springframework.context.support.GenericApplicationContext;
  * @author: BanTanger 半糖
  * @create: 2023-09-18 15:31
  */
-@SpringBootApplication(scanBasePackages = {
-        "com.bantanger.im.tcp",
-        // 这里是因为 feign 是在 tcp 层做的，需要被 tcp 层的 springboot 扫描管理
-        "com.bantanger.im.service.**.extensionpost.impl"
-})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        GenericApplicationContext context = new GenericApplicationContext();
-        // 允许Bean覆盖，后面的BeanDefinition能覆盖前面的
-        context.setAllowBeanDefinitionOverriding(true) ;
     }
 
 }
